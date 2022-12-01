464912be90cb493418ebca5374ac390409fce449
mlnchlc <84663706+mlnchlc@users.noreply.github.com>
2022-12-01 12:28:00 +0530

    complete first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rElements) {
 
+  let res = [];
+  let rSet = new Set();
+  for (const ele of rElements) {
+    rSet.add(ele);
+  }
+  for (const ele of arr) {
+    if (!rSet.has(ele))
+      res.push(ele);
+  }
+  return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
-};
+const sumAll = function(begin, end) {
+  if (end < begin) 
+    [begin, end] = [end, begin];
+  if (begin < 0 || !Number.isInteger(begin) || !Number.isInteger(end))
+    return "ERROR";
+  let sum = 0;
+  for (let i = begin; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
