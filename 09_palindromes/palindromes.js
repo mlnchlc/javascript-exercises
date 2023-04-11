7a182943953ef24eff6badcf3139889627073ce3
mlnchlc <84663706+mlnchlc@users.noreply.github.com>
2023-04-11 16:00:46 +0530

    do palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+function isLetter(c) {
+    return c.toLowerCase() != c.toUpperCase();
+}
 
+const palindromes = function (word) {
+    word = word.toUpperCase();
+    let right = word.length - 1;
+    let left = 0;
+
+    while (!isLetter(word.charAt(right))) right--;
+    while (!isLetter(word.charAt(left))) left++;
+
+    while (right > left) {
+        if (word.charAt(right) != word.charAt(left))
+            return false;
+        right--;
+        left++;
+        while (!isLetter(word.charAt(right))) right--;
+        while (!isLetter(word.charAt(left))) left++;
+    }
+    return true;
 };
 
 // Do not edit below this line
